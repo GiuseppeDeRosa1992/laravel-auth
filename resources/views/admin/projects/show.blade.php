9bbbdfcf019e3a84e45144b6ba78cf07b02dab3d
GiuseppeDeRosa1992 <derosagiuseppe1092@gmail.com>
2024-07-17 18:52:56 +0200

    change src img for show.blade.php and added modal for button delete

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -4,7 +4,7 @@
 	<div class="col-4 mt-3 m-auto">
 		<div class="card p-0 h-100">
 			<div class="col-12">
-				<img src="{{ $projects->img_preview }}" class="img-fluid rounded-start" alt="immagine-progetto">
+				<img src="{{ asset('storage/' . $projects->img_preview) }}" class="img-fluid rounded-start" alt="immagine-progetto">
 			</div>
 			<div class="row g-0 m-0">
 				<div class="col-12">
--- resources/views/partials/card.blade.php
+++ resources/views/partials/card.blade.php
@@ -5,7 +5,8 @@
 				<div class="card p-0 h-100">
 					<div class="row g-0 m-0">
 						<div class="col-12">
-							<img src="{{ asset('storage/' . $projectSingle->img_preview) }}" class="w-50 rounded-start" alt="immagine-progetto">
+							<img src="{{ asset('storage/' . $projectSingle->img_preview) }}" class="img-fluid rounded-start mb-2"
+								alt="immagine-progetto">
 						</div>
 						<h5 class="card-title"><b>Titolo Progetto:</b> {{ $projectSingle->title }}</h5>
 					</div>
@@ -15,16 +16,45 @@
 					<a href="{{ route('admin.projects.edit', $projectSingle->id) }}" class="btn btn-outline-primary my-2">Modifica
 						Progetto
 						<i class="fa-solid fa-pencil"></i></a>
-					{{-- CREO FORM PER CANCELLARE UN FUMETTO DAL DATABASE GLI DO LA ROTTA DESTROY E IL METODO POST POI SOTTO LO CAMBIO NEL METODO DELETE --}}
-					<form method="POST" action="{{ route('admin.projects.destroy', $projectSingle->id) }}">
-						@csrf
 
-						@method('DELETE')
-						<button type="submit" href="" class="btn btn-outline-danger my-2">Elimina
-							Progetto
-							<i class="fa-solid fa-trash-can"></i>
-						</button>
-					</form>
+					<!-- Modal trigger button -->
+					<button type="button" class="btn btn-outline-danger" data-bs-toggle="modal"
+						data-bs-target="#modal-{{ $projectSingle->id }}">Elimina Progetto
+						<i class="fa-solid fa-trash-can"></i>
+					</button>
+
+					<!-- Modal Body -->
+					<!-- if you want to close by clicking outside the modal, delete the last endpoint:data-bs-backdrop and data-bs-keyboard -->
+					<div class="modal fade" id="modal-{{ $projectSingle->id }}" tabindex="-1" data-bs-backdrop="static"
+						data-bs-keyboard="false" role="dialog" aria-labelledby="modalTitle-{{ $projectSingle->id }}" aria-hidden="true">
+						<div class="modal-dialog modal-dialog-scrollable modal-dialog-centered modal-sm" role="document">
+							<div class="modal-content">
+								<div class="modal-header">
+									<h5 class="modal-title" id="modalTitle-{{ $projectSingle->id }}">
+										Delete current project
+									</h5>
+									<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+								</div>
+
+								<div class="modal-body">
+									Deleting project name: {{ $projectSingle->title }}
+									⚡Danger, you cannot undo this operation
+								</div>
+								<div class="modal-footer justify-content-center">
+									{{-- CREO FORM PER CANCELLARE UN FUMETTO DAL DATABASE GLI DO LA ROTTA DESTROY E IL METODO POST POI SOTTO LO CAMBIO NEL METODO DELETE --}}
+									<form method="POST" action="{{ route('admin.projects.destroy', $projectSingle->id) }}">
+										@csrf
+
+										@method('DELETE')
+										<button type="submit" href="" class="btn btn-outline-danger my-2" data-bs-dismiss="modal">
+											<i class="fa-solid fa-trash-can"></i>
+										</button>
+									</form>
+
+								</div>
+							</div>
+						</div>
+					</div>
 				</div>
 			</div>
 		@endforeach
