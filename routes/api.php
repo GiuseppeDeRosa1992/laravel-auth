c2f7aca55c718d07898461de0f4117ec4cf7e30e
GiuseppeDeRosa1992 <derosagiuseppe1092@gmail.com>
2024-07-23 16:00:17 +0200

    added route /contacts inside api.php

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\LeadController;
 use App\Http\Controllers\Api\ProjectController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -27,3 +28,6 @@ Route::get('projects/latest', [ProjectController::class, 'latest']);
 //creo rotta per i dettagli del progetto
 Route::get('projects/{project:slug}', [ProjectController::class, 'show']);
 // Route::get('projects/{slug}', [ProjectController::class, 'show']); FUNZIONA UGUALE MA LA SINTASSI PIù GIUSTA è QUELLA SOPRA
+
+//AGGIUNGO LA ROTTA PER  IL FORM DI CONTATTO
+Route::post('/contacts', [LeadController::class, 'store']);
