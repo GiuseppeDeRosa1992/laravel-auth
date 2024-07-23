3266bb772e34c2e8c9829aa382684c2e747eb647
GiuseppeDeRosa1992 <derosagiuseppe1092@gmail.com>
2024-07-23 15:59:17 +0200

    implemented route mailable for lead

--- routes/web.php
+++ routes/web.php
@@ -40,6 +40,12 @@ Route::middleware(['auth'])
         Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
     });
 
+Route::get('/mailable', function () {
+    $lead = ['name' => 'Fabio', 'email' => 'fabio@example.com', 'message' => 'lorem ipsum dolor'];
+    // $lead = App\Models\Lead::first();
+    return new App\Mail\NewLeadMessage($lead);
+});
+
 //Auth::check()
 
 // public function index()
